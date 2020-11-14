019635b4bc5260a3b7204af91b49362b80cafba7
kastrupf <kastrupf@gmail.com>
2020-11-14 20:39:19 +0100

    création du script de migration de region et ajustes dans la table de ville

---
+++ src/main/resources/db/migration/V003__creation-table-region.sql
@@ -0,0 +1,17 @@
+create table region (
+	id bigint not null auto_increment,
+	nom varchar(80) not null,
+	
+primary key (id)
+) engine=InnoDB default charset=utf8;
+
+insert into region (nom) select distinct region from ville;
+
+alter table ville add column region_id bigint not null;
+
+update ville v set v.region_id = (select r.id from region r where r.nom = v.nom);
+
+alter table ville add constraint fk_ville_region foreign key (region_id) references region (id);
+
+alter table ville drop column region;
+
