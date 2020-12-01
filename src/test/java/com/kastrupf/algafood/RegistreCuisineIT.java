4e2619dfc63fa4e1e72f4caed42c2492d6444366
kastrupf <kastrupf@gmail.com>
2020-12-01 14:06:09 +0100

    Retour de l'état initial de la base de données pour chaque test avec callback du Flyway

--- src/test/java/com/kastrupf/algafood/RegistreCuisineIT.java
+++ src/test/java/com/kastrupf/algafood/RegistreCuisineIT.java
@@ -1,12 +1,13 @@
 package com.kastrupf.algafood;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.hasItems;
 import static org.hamcrest.Matchers.hasSize;
 
+import org.flywaydb.core.Flyway;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
@@ -22,11 +23,16 @@ public class RegistreCuisineIT {
 		@LocalServerPort
 		private int port;
 		
+		@Autowired
+		private Flyway flyway;
+		
 		@Before
 		public void setUp() {
 			RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
 			RestAssured.port = port;
 			RestAssured.basePath = "/cuisines";
+			
+			flyway.migrate();
 		}
 	
 	@Test
@@ -49,4 +55,17 @@ public class RegistreCuisineIT {
 			.body("", hasSize(4));
 //		.body("nom", hasItems("Française", "Italienne"));
 	}
+	
+	@Test
+	public void doitRetounerStatus201_QuandEnregistrerCuisine() {
+		given()
+			.body("{ \"nom\": \"Brésilienne\" }")
+			.contentType(ContentType.JSON)
+			.accept(ContentType.JSON)
+		.when()
+			.post()
+		.then()
+			.statusCode(HttpStatus.CREATED.value());
+	}
+	
 }
