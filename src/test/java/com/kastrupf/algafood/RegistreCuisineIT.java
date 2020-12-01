5bf88d9ee62ad0460937f74b7e8c7a175a5944d9
kastrupf <kastrupf@gmail.com>
2020-12-01 09:10:34 +0100

    Implémentation de tests API - registreCuisine avec REST Assured et validation du code d'état HTTP

--- src/test/java/com/kastrupf/algafood/RegistreCuisineIT.java
+++ src/test/java/com/kastrupf/algafood/RegistreCuisineIT.java
@@ -1,55 +1,35 @@
 package com.kastrupf.algafood;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static io.restassured.RestAssured.given;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import com.kastrupf.algafood.domain.exception.CuisineNonTrouveeException;
-import com.kastrupf.algafood.domain.exception.EntiteEnCoursUtilisationException;
-import com.kastrupf.algafood.domain.model.Cuisine;
-import com.kastrupf.algafood.domain.service.RegistreCuisineService;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class RegistreCuisineIT {
-
-	@Autowired
-	private RegistreCuisineService registreCuisine;
 	
-	@Test(expected = EntiteEnCoursUtilisationException.class)
-	public void neDoitPasSupprimerLaCuisine_QuandElleEstEnCoursDutilisation() {
-	    registreCuisine.supprimer(1L);
-	}
+		@LocalServerPort
+		private int port;
 	
-	@Test(expected = CuisineNonTrouveeException.class)
-	public void neDoitPasSupprimerLaCuisine_QuandElleEstInexistente() {
-		registreCuisine.supprimer(100L);
-	}    
-	  	
 	@Test
-	public void doitAjouterCuisine_QuandUnNomLuiEstAttibué() {
-		// scenario
-		Cuisine nouvelleCuisine = new Cuisine();
-		nouvelleCuisine.setNom("Chinoise");
-		
-		// action
-		nouvelleCuisine = registreCuisine.ajouter(nouvelleCuisine);
-		
-		// validation
-		assertThat(nouvelleCuisine).isNotNull();
-		assertThat(nouvelleCuisine.getId()).isNotNull();
+	public void doitRetounerStatus200_QuandRechercheCuisines() {
+		RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+				
+		given()
+			.basePath("/cuisines")
+			.port(port)
+			.accept(ContentType.JSON)
+		.when()
+			.get()
+		.then()
+			.statusCode(HttpStatus.OK.value());
 	}
-	
-//	@Test(expected = ConstraintViolationException.class)
-//	public void neDoitPasAjouterCuisine_QuandLeNomNestPasAttribué() {
-//		Cuisine nouvelleCuisine = new Cuisine();
-//		nouvelleCuisine.setNom(null);
-		
-//		nouvelleCuisine = registreCuisine.ajouter(nouvelleCuisine);
-//	}
 }
-
